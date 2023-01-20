b03c85894340fe8a9424382dbc3b5b5e350aed97
Harsh Sharma <harshsharma20503@gmail.com>
2023-01-20 15:37:57 +0530

    folder structuring and minor changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import "./index.css";
-import Home from "./routes/Home";
-import Team from "./routes/Team";
-import Mentor from "./routes/Mentor";
-import Contact from "./routes/Contact";
-import Alumni  from "./routes/Alumni";
-import './App.css';
 import { Route, Routes } from "react-router-dom";
+
+import Home from "./routes/Home/Home";
+import Team from "./routes/Team/Team";
+import Mentor from "./routes/Mentor/Mentor";
+import Contact from "./routes/Contact/Contact";
+import Alumni  from "./routes/Alumni/Alumni";
+
+import './App.css';
+import "./index.css";
+
 
 function App() {
   return (
