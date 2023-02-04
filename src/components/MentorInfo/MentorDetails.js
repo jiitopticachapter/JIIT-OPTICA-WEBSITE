415b8d92a1c6b194e9046174e429a2a91c6f3d86
Harsh Sharma <harshsharma20503@gmail.com>
2023-02-04 17:12:46 +0530

    shootingstars added

--- src/components/MentorInfo/MentorDetails.js
+++ src/components/MentorInfo/MentorDetails.js
@@ -22,7 +22,7 @@ const MentorDetails = () => {
       <div data-aos="fade-up">
         <section  class="about">
           <div className="main">
-            <img src="/images/anuraj_panwar.jpg" alt="Mentor photos" />
+            <img src="/images/mentors/anuraj_panwar.jpg" alt="Mentor photos" />
             <div className="about-text">
               <h2>Dr. ANURAJ PANWAR </h2>
               <h3>FACULTY ADVISOR</h3>
@@ -49,7 +49,7 @@ const MentorDetails = () => {
         <div data-aos="fade-up">
         <section class="about">
           <div className="main">
-            <img src="/images/manoj_tripathi.png" alt="Mentor photos" />
+            <img src="/images/mentors/manoj_tripathi.png" alt="Mentor photos" />
             <div className="about-text">
               <h2>Dr. Manoj Tripathi </h2>
               <h3> CO-FACULTY ADVISOR</h3>
@@ -72,7 +72,7 @@ const MentorDetails = () => {
         <div data-aos="fade-up">
         <section class="about">
           <div className="main">
-            <img src="/images/ashish_bhatnagar.jpg" alt="Mentor photos" />
+            <img src="/images/mentors/ashish_bhatnagar.jpg" alt="Mentor photos" />
             <div className="about-text">
               <h2>Dr. ASHISH BHATNAGAR</h2>
               <h3>  CO-FACULTY ADVISOR</h3>
