fd73233002631b8d1536fc0e0f104180b0307756
teesha17 <teeshu.2179@gmail.com>
2024-03-20 22:06:24 +0530

    added images

--- src/components/Gallery/GalleryimgData.js
+++ src/components/Gallery/GalleryimgData.js
@@ -1,198 +1,233 @@
-const recenthighlights=[
-  {
-     imgUrl:"/images/recent highlights/gallery1.jpg"
-  },
+const recenthighlights = [
+    {
+        imgUrl: "/images/recent highlights/IMG_2385.jpg"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2391.jpg"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2393.jpg"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2395.jpg"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2398.jpg"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2414.jpg"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2423.jpg"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2431.jpg"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2871.JPG"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2974.JPG"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_2977.JPG"
+    },
+    {
+        imgUrl: "/images/recent highlights/IMG_3001.JPG"
+    },
+    {
+        imgUrl: "/images/recent highlights/gallery1.jpg"
+    },
 
-  {
-     imgUrl:"/images/recent highlights/gallery2.jpg"
-  },
+    {
+        imgUrl: "/images/recent highlights/gallery2.jpg"
+    },
 
-  {
-     imgUrl:"/images/recent highlights/gallery3.jpg"
-  },
+    {
+        imgUrl: "/images/recent highlights/gallery3.jpg"
+    },
 
-  {
-     imgUrl:"/images/recent highlights/gallery4.jpg"
-  },
+    {
+        imgUrl: "/images/recent highlights/gallery4.jpg"
+    },
 
-  {
-    imgUrl: "/images/recent highlights/gallery5.jpeg"
-  },
+    {
+        imgUrl: "/images/recent highlights/gallery5.jpeg"
+    },
 
-  {
-    imgUrl: "/images/recent highlights/gallery6.jpeg"
-  }, 
+    {
+        imgUrl: "/images/recent highlights/gallery6.jpeg"
+    },
 
-  {
-    imgUrl: "/images/recent highlights/gallery7.jpeg"
-  },
-  
-  {
-    imgUrl: "/images/recent highlights/gallery8.jpeg"
-  },
+    {
+        imgUrl: "/images/recent highlights/gallery7.jpeg"
+    },
 
-]
-
-const gallery2023=[
     {
-        original:"/images/gallery images/2023 gallery/2023a.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023a.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023b.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023b.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023c.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023c.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023d.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023d.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023e.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023e.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023f.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023f.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023g.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023g.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023h.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023h.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023i.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023i.jpg",
-    },
-    {
-        original:"/images/gallery images/2023 gallery/2023j.jpg",
-        thumbnail:"/images/gallery images/2023 gallery/2023j.jpg",
+        imgUrl: "/images/recent highlights/gallery8.jpeg"
     },
 ]
 
-const gallery2022=[
+const gallery2023 = [
     {
-        original:"/images/gallery images/2022 gallery/2022.jpeg",
-        thumbnail:"/images/gallery images/2022 gallery/2022.jpeg",
+        original: "/images/gallery images/2023 gallery/2023a.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023a.jpg",
     },
     {
-        original:"/images/gallery images/2022 gallery/2022a.jpeg",
-        thumbnail:"/images/gallery images/2022 gallery/2022a.jpeg",
+        original: "/images/gallery images/2023 gallery/2023b.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023b.jpg",
     },
     {
-        original:"/images/gallery images/2022 gallery/2022b.jpeg",
-        thumbnail:"/images/gallery images/2022 gallery/2022b.jpeg",
+        original: "/images/gallery images/2023 gallery/2023c.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023c.jpg",
     },
     {
-        original:"/images/gallery images/2022 gallery/2022c.jpeg",
-        thumbnail:"/images/gallery images/2022 gallery/2022c.jpeg",
+        original: "/images/gallery images/2023 gallery/2023d.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023d.jpg",
     },
     {
-        original:"/images/gallery images/2022 gallery/2022d.jpeg",
-        thumbnail:"/images/gallery images/2022 gallery/2022d.jpeg",
-    },
-]
-
-const gallery2021=[
-    {
-        original:"/images/gallery images/2021 gallery/2021a.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021a.jpeg",
+        original: "/images/gallery images/2023 gallery/2023e.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023e.jpg",
     },
     {
-        original:"/images/gallery images/2021 gallery/2021b.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021b.jpeg",
+        original: "/images/gallery images/2023 gallery/2023f.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023f.jpg",
     },
     {
-        original:"/images/gallery images/2021 gallery/2021c.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021c.jpeg",
+        original: "/images/gallery images/2023 gallery/2023g.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023g.jpg",
     },
     {
-        original:"/images/gallery images/2021 gallery/2021d.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021d.jpeg",
+        original: "/images/gallery images/2023 gallery/2023h.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023h.jpg",
     },
     {
-        original:"/images/gallery images/2021 gallery/2021e.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021e.jpeg",
+        original: "/images/gallery images/2023 gallery/2023i.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023i.jpg",
     },
     {
-        original:"/images/gallery images/2021 gallery/2021f.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021f.jpeg",
-    },
-    {
-        original:"/images/gallery images/2021 gallery/2021g.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021g.jpeg",
-    },
-    {
-        original:"/images/gallery images/2021 gallery/2021h.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021h.jpeg",
-    }, 
-    {
-        original:"/images/gallery images/2021 gallery/2021i.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021i.jpeg",
-    },
-    {
-        original:"/images/gallery images/2021 gallery/2021j.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021j.jpeg",
-    },
-    {
-        original:"/images/gallery images/2021 gallery/2021k.jpeg",
-        thumbnail:"/images/gallery images/2021 gallery/2021k.jpeg",
-    },
-    
-]
-
-const gallery2019=[
-    {
-        original:"/images/gallery images/2019 gallery/2019.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019a.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019a.jpeg",
-    },
-
-    {
-        original:"/images/gallery images/2019 gallery/2019b.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019b.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019c.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019c.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019d.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019d.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019e.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019e.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019f.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019f.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019g.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019g.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019h.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019h.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019i.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019i.jpeg",
-    },
-    {
-        original:"/images/gallery images/2019 gallery/2019j.jpeg",
-        thumbnail:"/images/gallery images/2019 gallery/2019j.jpeg",
+        original: "/images/gallery images/2023 gallery/2023j.jpg",
+        thumbnail: "/images/gallery images/2023 gallery/2023j.jpg",
     },
 ]
 
-export {gallery2023,gallery2022,gallery2019,gallery2021,recenthighlights};
+const gallery2022 = [
+    {
+        original: "/images/gallery images/2022 gallery/2022.jpeg",
+        thumbnail: "/images/gallery images/2022 gallery/2022.jpeg",
+    },
+    {
+        original: "/images/gallery images/2022 gallery/2022a.jpeg",
+        thumbnail: "/images/gallery images/2022 gallery/2022a.jpeg",
+    },
+    {
+        original: "/images/gallery images/2022 gallery/2022b.jpeg",
+        thumbnail: "/images/gallery images/2022 gallery/2022b.jpeg",
+    },
+    {
+        original: "/images/gallery images/2022 gallery/2022c.jpeg",
+        thumbnail: "/images/gallery images/2022 gallery/2022c.jpeg",
+    },
+    {
+        original: "/images/gallery images/2022 gallery/2022d.jpeg",
+        thumbnail: "/images/gallery images/2022 gallery/2022d.jpeg",
+    },
+]
+
+const gallery2021 = [
+    {
+        original: "/images/gallery images/2021 gallery/2021a.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021a.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021b.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021b.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021c.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021c.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021d.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021d.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021e.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021e.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021f.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021f.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021g.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021g.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021h.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021h.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021i.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021i.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021j.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021j.jpeg",
+    },
+    {
+        original: "/images/gallery images/2021 gallery/2021k.jpeg",
+        thumbnail: "/images/gallery images/2021 gallery/2021k.jpeg",
+    },
+
+]
+
+const gallery2019 = [
+    {
+        original: "/images/gallery images/2019 gallery/2019.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019a.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019a.jpeg",
+    },
+
+    {
+        original: "/images/gallery images/2019 gallery/2019b.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019b.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019c.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019c.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019d.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019d.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019e.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019e.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019f.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019f.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019g.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019g.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019h.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019h.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019i.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019i.jpeg",
+    },
+    {
+        original: "/images/gallery images/2019 gallery/2019j.jpeg",
+        thumbnail: "/images/gallery images/2019 gallery/2019j.jpeg",
+    },
+]
+
+export { gallery2023, gallery2022, gallery2019, gallery2021, recenthighlights };
